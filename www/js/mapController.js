136c14b14cde049077e9630803e4ceb9d94d422c
josegh <josegh@josegh-Lenovo-B40-80>
2016-11-01 18:00:13 -0500

    Se guarda el cambio

--- www/js/mapController.js
+++ www/js/mapController.js
@@ -6,15 +6,10 @@
 angular.module('starter.controllers').controller('MapCtrl', function ($rootScope, $ionicHistory, $scope, $location, $state, $stateParams, $route, $templateCache, $http) {
 
     $ionicHistory.clearCache();
-    $ionicHistory.nextViewOptions({
-        disableBack: true
-    });
 
     $route.reload();
     $templateCache.removeAll();
-
     var source = new ol.source.Vector();
-
     var gmap = new google.maps.Map(document.getElementById('gmap'), {
         disableDefaultUI: true,
         keyboardShortcuts: false,
@@ -24,13 +19,10 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
         streetViewControl: false,
         mapTypeId: google.maps.MapTypeId.SATELLITE
     });
-
     $scope.icono_5 = 'icon ion-map';
     $scope.icono_4 = 'icon ion-ios-crop-strong';
     $scope.icono_3 = 'icon ion-ios-location';
     $scope.centroIcono = 'icon ion-plus';
-
-
 //    vector de dibujo
     var vectorDibujo = new ol.layer.Vector({
         name: 'my_vectorlayer',
@@ -43,25 +35,11 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
         })
     });
 
-//se dibuja el radio
-    if ($stateParams.placa == '0') {
-
-        var precisionCircle = ol.geom.Polygon.circular(
-                /* WGS84 Sphere */
-                new ol.Sphere(6378137),
-                [-74.5971431341708, 6.5323780468158],
-                1000,
-                /* Number of verticies */
-                64).transform('EPSG:4326', 'EPSG:3857');
-        var precisionCircleFeature = new ol.Feature(precisionCircle)
-
-    }
 
     $scope.buscar = function () {
 
         $state.go('app.browse');
     };
-
     function addInteraction() {
 
         draw = new ol.interaction.Draw({
@@ -69,7 +47,6 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
             type: /** @type {ol.geom.GeometryType} */ (typoStr)
         });
         map.addInteraction(draw);
-
     }
 
 
@@ -81,7 +58,6 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
                 btn4_bol = false;
                 typoStr = 'Polygon';
                 addInteraction();
-
             } else {
                 btn4_bol = true;
                 map.removeInteraction(draw);
@@ -89,7 +65,6 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
         } else if (menu == 2) {
 
             $state.go('app.search');
-
         } else if (menu == 3) {
 
             if (zonas_exc == null) {
@@ -106,15 +81,12 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
                     })
                 });
                 map.addLayer(zonas_exc);
-
             } else {
                 map.removeLayer(zonas_exc);
                 zonas_exc = null;
             }
         }
     };
-
-
     $scope.mostrarFinder = function () {
 
         if (menu == 1) {
@@ -146,7 +118,6 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
             }
         }
     };
-
     $scope.mostrarExp = function () {
 
         $scope.mostrar2 = false;
@@ -157,7 +128,6 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
         menu = 3;
         $scope.funcion5 = "mostrarTitulos()";
     };
-
     $scope.mostrarMenu = function () {
 
         if (menu == 1) {
@@ -180,7 +150,6 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
             menu = 1;
         }
     };
-
     $scope.cambiarMapa = function () {
 
         if (menu == 1) {
@@ -217,15 +186,12 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
 
         }
     };
-
     var view = new ol.View({
         // make sure the view doesn't go beyond the 22 zoom levels of Google Maps
         center: ol.proj.fromLonLat([-74.5981636036184, 6.25468647083332]),
         zoom: 7,
         maxZoom: 21
     });
-
-
     view.on('change:center', function () {
         var center = ol.proj.transform(view.getCenter(), 'EPSG:3857', 'EPSG:4326');
         gmap.setCenter(new google.maps.LatLng(center[1], center[0]));
@@ -233,12 +199,42 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
     view.on('change:resolution', function () {
         gmap.setZoom(view.getZoom());
     });
-
     var olMapDiv = document.getElementById('olmap');
-
     var vector = new ol.layer.Vector({
     });
+    
+    var vectorPunto = new ol.layer.Vector();
+    
+    if ($stateParams.radio != undefined && $stateParams.radio != "" && $stateParams.coorX != undefined && $stateParams.coorX != "" && $stateParams.coorY != undefined && $stateParams.coorY != "") {
+        
+        var circle = new ol.geom.Circle(ol.proj.transform([parseFloat($stateParams.coorX), parseFloat($stateParams.coorY)], 'EPSG:4326', 'EPSG:3857'), parseInt($stateParams.radio));
 
+        var CircleFeature = new ol.Feature(circle);
+
+
+        var vectorSource = new ol.source.Vector({
+           dataProjection: 'EPSG:4326',
+           featureProjection: 'EPSG:3857'
+        });
+
+        vectorSource.addFeatures([CircleFeature]);
+
+
+        vectorPunto = new ol.layer.Vector({
+            source: vectorSource,
+            style: [
+                new ol.style.Style({
+                    stroke: new ol.style.Stroke({
+                        color: 'blue',
+                        width: 3
+                    }),
+                    fill: new ol.style.Fill({
+                        color: 'rgba(0, 0, 255, 0.1)'
+                    })
+                })]
+        });
+
+    }
 
 
     if ($stateParams.placa != undefined && $stateParams.placa != "") {
@@ -252,16 +248,13 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
             }
         }).then(function successCallback(response) {
 
-            console.log(response.data[0]);
+//            console.log(response.data[0]);
             wkt = response.data[0].coordenadas;
-
             var format = new ol.format.WKT();
-
             var feature = format.readFeature(wkt, {
                 dataProjection: 'EPSG:4326',
                 featureProjection: 'EPSG:3857'
             });
-
             vector = new ol.layer.Vector({
                 source: new ol.source.Vector({
                     features: [feature]
@@ -287,11 +280,10 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
                     })
                 })
             });
-
             var extent = feature.getGeometry().getExtent();
-            console.log(extent);
+//            console.log(extent);
             map = new ol.Map({
-                layers: [vector, vectorDibujo],
+                layers: [vector, vectorDibujo, vectorPunto],
                 interactions: ol.interaction.defaults({
                     altShiftDragRotate: false,
                     dragPan: false,
@@ -301,26 +293,40 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
                 target: olMapDiv,
                 view: view
             });
-
             map.getView().fit(extent, map.getSize());
-
             map.on('dblclick', function (evt) {
                 console.log('doubled');
                 var feature = map.forEachFeatureAtPixel(evt.pixel,
                         function (feature, layer) {
-                            
-                            // si entra aqui es por que tiene datos para mostrar
-                            $state.go('app.playlists', {placa: item}, {reload: true});
-                        });                        
-            });
 
+                            // si entra aqui es por que tiene datos para mostrar
+                            console.log('emtro');
+                            console.log(response.data[0].area_hec);
+                            $state.go('app.playlist',
+                                    {
+                                        area_hec: response.data[0].area_hec,
+                                        estado_juridico: response.data[0].estado_juridico,
+                                        fecha_radica_inscribe: response.data[0].fecha_radica_inscribe,
+                                        minerales: response.data[0].minerales,
+                                        modalidad: response.data[0].modalidad,
+                                        municipios: response.data[0].municipios,
+                                        personas: response.data[0].personas,
+                                        placa: response.data[0].placa,
+                                        tipo_expediente: response.data[0].tipo_expediente
+                                    }
+                            ,
+                                    {
+                                        reload: true
+                                    }
+                            );
+                        });
+            });
 //            gmap.setCenter(new google.maps.LatLng('6.25468647083332', '-74.5981636036184'));
 //            gmap.setZoom(7);
 
 
             olMapDiv.parentNode.removeChild(olMapDiv);
             gmap.controls[google.maps.ControlPosition.TOP_LEFT].push(olMapDiv);
-
         }, function errorCallback(response) {
 
             alert('Error de comunicacion, consulte su WebMaster');
@@ -328,7 +334,7 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
     } else {
 
         map = new ol.Map({
-            layers: [vector, vectorDibujo],
+            layers: [vector, vectorDibujo, vectorPunto],
             interactions: ol.interaction.defaults({
                 altShiftDragRotate: false,
                 dragPan: false,
@@ -337,14 +343,10 @@ angular.module('starter.controllers').controller('MapCtrl', function ($rootScope
             target: olMapDiv,
             view: view
         });
-
         gmap.setCenter(new google.maps.LatLng('6.25468647083332', '-74.5981636036184'));
         gmap.setZoom(7);
-
         olMapDiv.parentNode.removeChild(olMapDiv);
         gmap.controls[google.maps.ControlPosition.TOP_LEFT].push(olMapDiv);
-
-
     }
 
 });
